8bdac94ec6ccd4b0ea5b962f52a0607f48a3c4f4
marwen maliou <158064032+maliou125@users.noreply.github.com>
2024-03-09 10:51:57 +0100

    changing color check point js 2

---
+++ main.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+var colorBox = document.getElementById('color-box');
+var changeColorBtn = document.getElementById('change-color-btn');
+
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+changeColorBtn.addEventListener('click', function() {
+    colorBox.style.backgroundColor = getRandomColor();
+});
+});
